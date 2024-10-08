3324c28c2ba28b67d40e12216312e1a2789f93c5
Sardius <0xsardius@gmail.com>
2024-10-08 19:00:58 -0400

    Guess component refactor

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Guess from "../Guess";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { range } from "../../utils";
 
 function GuessResults({ guesses }) {
   return (
     <div class="guess-results">
-      {guesses.map((guess, index) => (
-        <p key={index} class="guess">
-          {guess}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} guess={guesses[num]} />
       ))}
     </div>
   );
