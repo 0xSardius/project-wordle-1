189a5df034581db1a790a0164dfd28c16a76e129
Sardius <0xsardius@gmail.com>
2024-10-08 20:35:43 -0400

    Exercise 4 complete

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ guess }) {
+function Guess({ guess, answer }) {
+  const result = checkGuess(guess, answer);
+
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
+        <span key={num} className={`cell ${result ? result[num].status : ""}`}>
           {guess ? guess[num] : undefined}
         </span>
       ))}
